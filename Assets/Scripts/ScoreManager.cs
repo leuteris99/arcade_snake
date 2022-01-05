0656a13411246c30635f5ce6f5335e473bd12072
Lefteris Alexiou <leualexiou@gmail.com>
2022-01-05 18:51:31 +0200

    finishing the menus

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
 
     public Text scoreText; // the ui score element.
+    public Text targetText;
+    public int endScore = 15;
 
-    int score = 0;
-    string printingScoreString = " Points";
+    private int score = 0;
+    private string target;
+    readonly string printingScoreString = " Points";
+    readonly string printingTargetString = "Target: ";
 
     private void Awake() {
         instance = this;
@@ -19,10 +24,46 @@ public class ScoreManager : MonoBehaviour
     void Start()
     {
         scoreText.text = score.ToString() + printingScoreString;
+        target = GetRandomTarget();
+        targetText.text =  printingTargetString + target;
     }
 
-    public void addPoint(){
-        score += 1;
-        scoreText.text = score.ToString() + printingScoreString;
+    public void AddPoint(int pointNum){
+        if(AddPointChecker(pointNum).Equals(target)){
+            score += 1;
+            scoreText.text = score.ToString() + printingScoreString;
+            if(score >= endScore)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+
+    private string AddPointChecker(int pointNum)
+    {
+        if(pointNum % 2 == 0)
+        {
+            return "evens";
+        }
+        return "odds";
+    }
+
+    string GetRandomTarget()
+    {
+        if (RandomBoolean())
+        {
+            return "odds";
+        }
+        return "evens";
+    }
+
+    bool RandomBoolean() {
+        {
+            if (Random.value >= 0.5)
+            {
+                return true;
+            }
+            return false;
+        } 
     }
 }
